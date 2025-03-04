ebd6e8e440e307027ea8bc63d27fab2dddc92225
bhashimoto <brunothashimoto@gmail.com>
2025-03-04 11:48:45 -0300

    more js

--- test.js
+++ test.js
@@ -1,13 +1,25 @@
-function changeText() {
-	console.log("hello")
-	h2 = document.getElementById("targetText")
-	h2.textContent = "pass"
-}
-
 function loadData() {
 	fetch("./data.json")
 		.then(response => response.json())
 		.then(data => console.log(data))
 		.catch(error => console.error('Error fetching JSON', error));
-}
+};
 
+function buildTable(elementId, data) {
+	table = `<thead>
+			<td>Nome</td>
+			<td>Tipo</td>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Picanha</td>
+				<td>bovina</td>
+			</tr>
+			<tr>
+				<td>Costela</td>
+				<td>suína</td>
+			</tr>
+		</tbody>`
+	element = document.getElementById(elementId)
+	element.value = table
+}
