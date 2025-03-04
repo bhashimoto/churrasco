6cde3f87f75a26714728fd5a0f6f5180d10cb464
bhashimoto <brunothashimoto@gmail.com>
2025-03-04 11:38:23 -0300

    add data

--- test.js
+++ test.js
@@ -3,3 +3,11 @@ function changeText() {
 	h2 = document.getElementById("targetText")
 	h2.textContent = "pass"
 }
+
+function loadData() {
+	fetch("./data.json")
+		.then(response => response.json())
+		.then(data => console.log(data))
+		.catch(error => console.error('Error fetching JSON', error));
+}
+
